4334d4292175666cfc7696ac3455133f4c118468
Brad Messer <messerb5467@gmail.com>
2020-12-23 19:32:34 -0500

    Initial test and freeze of python environment.

---
+++ setup-env.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+conda install -c anaconda scikit-learn -y
+conda install -c anaconda pandas -y
+conda install -c anaconda matplotlib -y
+conda install -c anaconda seaborn -y
+conda install -c anaconda statsmodels -y 
+conda install -c conda-forge xgboost -y
+conda install -c anaconda tensorflow-estimator -y
+conda install -c anaconda keras -y
+conda install -c pytorch pytorch -y
+conda install -c main pip -y
+conda install -c anaconda jupyter_core -y
+conda install -c conda-forge notebook -y
+yes | pip install kaggle
